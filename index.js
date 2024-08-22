b7d365228d99bfeb2dfcf0e71315126fce000bd1
Dev Muji <140706113+MUJTABA4060@users.noreply.github.com>
2024-08-22 17:17:02 +0500

    chnage the js file

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo");
 const links = document.querySelector(".links");
 const btn = document.querySelector(".btn");
+
+logo.addEventListener("click", () => {
+  open ("./index.html")
+})
