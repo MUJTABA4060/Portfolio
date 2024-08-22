53b56c9c2e1bf8596381aa3d44b63c7cb04fafc4
Dev Muji <140706113+MUJTABA4060@users.noreply.github.com>
2024-08-22 17:14:13 +0500

    change the js

---
+++ index.js
@@ -0,0 +1,4 @@
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
+const links = document.querySelector(".links");
+const btn = document.querySelector(".btn");
