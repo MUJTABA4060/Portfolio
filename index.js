fd1a2e4d385eea5c9bd2f9c162b17f86ea2ba826
Dev Muji <140706113+MUJTABA4060@users.noreply.github.com>
2024-08-28 15:10:26 +0500

    delte al  the files

--- index.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.querySelector(".navbar");
-const logo = document.querySelector("#logo");
-const links = document.querySelector(".links");
-const btn = document.querySelector(".btn");
-
-logo.addEventListener("click", () => {
-  open ("./index.html")
-})
-
