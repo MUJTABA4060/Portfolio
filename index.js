33fa54b7a9a935654b145a8bfacc5e76e966b2f8
Dev Muji <140706113+MUJTABA4060@users.noreply.github.com>
2024-08-28 15:09:58 +0500

    change the logo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const navbar = document.querySelector(".navbar");
-const logo = document.querySelector(".logo");
+const logo = document.querySelector("#logo");
 const links = document.querySelector(".links");
 const btn = document.querySelector(".btn");
 
@@ -7,10 +7,3 @@ logo.addEventListener("click", () => {
   open ("./index.html")
 })
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    navbar.classList.add("active");
-  } else {
-    navbar.classList.remove("active");
-  }
-})
