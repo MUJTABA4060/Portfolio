97be013fd6d0491822a00a5cba124b3526abe163
Dev Muji <140706113+MUJTABA4060@users.noreply.github.com>
2024-08-29 17:43:43 +0500

    change the visibility

---
+++ index.js
@@ -0,0 +1,7 @@
+const navLinks = document.getElementById("navbar-links");
+
+document.getElementById("menu").addEventListener("click", () => {
+
+    navbar-Links.classList.toggle("active");
+    document.getElementById("menu").classList.toggle("change");
+})
